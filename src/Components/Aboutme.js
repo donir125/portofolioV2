3db5c173390bdc6908606a70ebe20f753879a00c
Pgirardin <45573924+Pgirardin@users.noreply.github.com>
2022-07-26 18:28:12 +0300

    Update Aboutme.js

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -4,7 +4,7 @@ class Aboutme extends Component {
   render() {
     return (
       <div style={{ textAlign: "center", marginLeft: 'auto', color: '#fff', marginRight: 'auto', marginTop: '100px', width: '60%', background: 'gray', padding: '20px' }}>
-        <h1 style={{ fontSize: '50px', textTransform: 'uppercase', fontFamily: 'Anton, sans-serif' }}>Nihorimbere Belyse</h1>
+        <h1 style={{ fontSize: '50px', textTransform: 'uppercase', fontFamily: 'Anton, sans-serif' }}>Belyse N</h1>
         <p style={{ textAlign: 'justify', fontFamily: 'Oxygen, sans-serif' }}>
           This is Nihorimbere Belyse.  I’m an enthusiastic, responsible, self-motivated and hard-working person.
           I’m able to work well under pressure. 
