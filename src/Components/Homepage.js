bdbc6d77616b40b60f231a9f9f2451d9d1cb7c2c
Pgirardin <45573924+Pgirardin@users.noreply.github.com>
2022-07-26 17:56:02 +0300

    Add files via upload

---
+++ src/Components/Homepage.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="homepage-grid">
+          <Cell col={12}>
+            <div className="profile-img-border">
+              <img
+                className="profile-img"
+                src="images/DSC_5211-00.jpg"
+                alt="Ismail"
+              />
+            </div>
+            <div className="banner-text">
+              <h1>pharmacist</h1>
+              <hr />
+              <p>
+                I am Nihorimbere Belyse, experienced member of the healthcare  with skills in time management and interpersonal communication
+                ; I have extensive knowledge in filling prescriptions, reviewing medical history records and identifying potential drug interactions
+           
+              </p>
+        
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
